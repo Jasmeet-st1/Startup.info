78a8d02de6bfde1267673ebfa2fcbe72f2bac0fe
Jasmeet Singh <jasmeetst1@gmail.com>
2023-12-20 18:26:03 +0530

    Update List.jsx
    changed rendering logic for loading screen

--- frontend/src/Components/List.jsx
+++ frontend/src/Components/List.jsx
@@ -26,6 +26,10 @@ export default function List({ list, isLoading, setIsLoading }) {
         )
     }
 
+    if (isLoading) {
+        return LoadingScreen();
+    }
+
     useEffect(() => {
         setPage(1);
     }, [list]);
@@ -45,11 +49,7 @@ export default function List({ list, isLoading, setIsLoading }) {
 
     if (list.length === 0) {
         return (
-            <>
-                {isLoading ? LoadingScreen() :
-                    <p className="text-xl md:text-2xl lg:text-3xl text-center text-red-700">No Data Found. Please Try with some other filter.</p>
-                }
-            </>
+            <p className="text-xl md:text-2xl lg:text-3xl text-center text-red-700">No Data Found. Please Try with some other filter.</p>
         )
     }
 
@@ -75,29 +75,24 @@ export default function List({ list, isLoading, setIsLoading }) {
     }
 
     return (
-        // LoadingScreen()
-        <>
-            {isLoading ? LoadingScreen() :
-                <div className="px-3 md:px-5 lg:px-9">
+        <div className="px-3 md:px-5 lg:px-9">
 
-                    <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1'>
 
-                        {
-                            records.map((item, i) => {
-                                return <Card key={i} item={item} />
-                            })
-                        }
-                    </div>
+                {
+                    records.map((item, i) => {
+                        return <Card key={i} item={item} />
+                    })
+                }
+            </div>
 
-                    <div className="w-max mx-auto my-3">
+            <div className="w-max mx-auto my-3">
 
-                        <button className={`${page === 1 ? 'cursor-not-allowed bg-blue-400 dark:bg-blue-500' : 'bg-blue-700 hover:bg-blue-800'} text-white font-medium rounded-lg text-sm px-3 py-2 mx-2 mb-2 focus:outline-none`} onClick={prev}>Previous</button>
-                        <span>Page {page}/{totPages}</span>
-                        <button className={`${page === totPages ? 'cursor-not-allowed bg-blue-400 dark:bg-blue-500' : 'bg-blue-700 hover:bg-blue-800'} text-white font-medium rounded-lg text-sm px-3 py-2 mx-2 mb-2 focus:outline-none`} onClick={next}>Next</button>
-                    </div>
-                </div>
-            }
-        </>
+                <button className={`${page === 1 ? 'cursor-not-allowed bg-blue-400 dark:bg-blue-500' : 'bg-blue-700 hover:bg-blue-800'} text-white font-medium rounded-lg text-sm px-3 py-2 mx-2 mb-2 focus:outline-none`} onClick={prev}>Previous</button>
+                <span>Page {page}/{totPages}</span>
+                <button className={`${page === totPages ? 'cursor-not-allowed bg-blue-400 dark:bg-blue-500' : 'bg-blue-700 hover:bg-blue-800'} text-white font-medium rounded-lg text-sm px-3 py-2 mx-2 mb-2 focus:outline-none`} onClick={next}>Next</button>
+            </div>
+        </div>
     )
 
 }
